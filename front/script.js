471a071db811d353d93ea5634674b3d185fed619
vik <freitasvitoria712@gmail.com>
2026-02-28 19:53:36 -0300

    [add] render

--- front/script.js
+++ front/script.js
@@ -13,7 +13,7 @@ form.onsubmit = async (e) => {
   submitBtn.disabled = true;
 
   try {
-    const response = await fetch("http://127.0.0.1:5000/download", {
+    const response = await fetch("https://meu-downloader-m0x7.onrender.com/download", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
